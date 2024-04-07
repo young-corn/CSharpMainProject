fa64b9a8308b99a2a76f06d7d711fd05385275ca
Young corn <chernousov99@gmail.com>
2024-04-08 03:37:01 +0600

    gfdd

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,13 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+    //
+    /*/
+     * 
+     * 
+     * 
+     * 
+    /*/
     void Start()
     {
         Time.timeScale = _timeScale;
